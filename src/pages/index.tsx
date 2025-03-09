486388177d64727ebb7558bb41a708db202d7eb4
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-03-09 17:01:09 +1100

    feat: enhance home page with introduction section and call-to-action buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,21 @@ import styles from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <main className={styles.container}>
+      <section className="home-introduction-section">
+        <div className="home-introduction-section-content">
+          <h1>Sonna Studios</h1>
+          <h2>Photography & Videography</h2>
+          <p>Make an Enquiry</p>
+          <p>
+            Sonna is a premium event photo & video coverage for Australia & New
+            Zealand, capturing the biggest and best events in town. Working
+            alongside global and local brands alike, Sonna is the ‘go-to’ event
+            photographer and videographer in NZ and a preferred supplier to
+            government departments, charities, businesses, brands and
+            corporations.
+          </p>
+        </div>
+      </section>
       <div className="home-image-text-module">
         <p>
           We deliver premium event photo & video coverage New Zealand wide,
@@ -35,6 +50,17 @@ export default function Home() {
           in NZ and a preferred supplier to government departments, charities,
           businesses, brands and corporations.
         </p>
+        <button>Book A Shoot</button>
+      </div>
+      <div className="home-sticky-scroll-photography-module">
+        <h3>
+          We deliver premium event photo & video coverage New Zealand wide,
+          capturing the biggest and best events in town. Working alongside
+          global and local brands alike, Sonna is the ‘go-to’ event photographer
+          and videographer in NZ and a preferred supplier to government
+          departments, charities, businesses, brands and corporations.
+        </h3>
+        <button>View All Photography</button>
       </div>
     </main>
   );
