59eba35cfbdbb48cd77ff8b271922ccb342bc5d9
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-03-09 16:46:16 +1100

    feat: update home page structure and content for improved presentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,19 @@ import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <div className="home-image-text-module">
         <p>We deliver premium event photo & video coverage New Zealand wide, capturing the biggest and best events in town. Working alongside global and local brands alike, Sonna is the ‘go-to’ event photographer and videographer in NZ and a preferred supplier to government departments, charities, businesses, brands and corporations.</p>
       </div>
       <div className="home-medium-paragraph-module">
-        
+        <h2>
+      We deliver premium event photo & video coverage New Zealand wide, capturing the biggest and best events in town. Working alongside global and local brands alike, Sonna is the ‘go-to’ event photographer and videographer in NZ and a preferred supplier to government departments, charities, businesses, brands and corporations.
+      </h2>
       </div>
-    </div>
+      <div className="home-image-carousel-module"></div>
+      <div className="home-sticky-book-a-shoot-module">
+        <p>We deliver premium event photo & video coverage New Zealand wide, capturing the biggest and best events in town. Working alongside global and local brands alike, Sonna is the ‘go-to’ event photographer and videographer in NZ and a preferred supplier to government departments, charities, businesses, brands and corporations.We deliver premium event photo & video coverage New Zealand wide, capturing the biggest and best events in town. Working alongside global and local brands alike, Sonna is the ‘go-to’ event photographer and videographer in NZ and a preferred supplier to government departments, charities, businesses, brands and corporations.</p>
+      </div>
+    </main>
   );
 }
